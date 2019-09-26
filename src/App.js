ca971cd105365cc824c9a33eede32752ceba9b28
Gustavo Malamud <gustavo.malamud@gmail.com>
2019-09-26 17:13:40 -0300

    Modify dispatchers, actions, reducers, etc

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { addCounter, subtractCounter, clearCounter } from './store';
 import './App.css';
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App" >
-        <p>Hola CourseIt - {this.props.counter}</p>
-      </div>)
-  };
+	handleClick() {
+		const { dispatch } = this.props;
+		dispatch(addCounter());
+	}
+	minusClick() {
+		const { dispatch } = this.props;
+		dispatch(subtractCounter());
+	}
+	clearCounter() {
+		const { dispatch } = this.props;
+		dispatch(clearCounter());
+	}
+
+	render() {
+		return (
+			<div className='App'>
+				<p>Hola CourseIt - </p>
+				<p>Counter : {this.props.counter}</p>
+				<p>Nombre: {this.props.test}</p>
+				<button onClick={() => this.handleClick()}>Sumar</button>
+				<button onClick={() => this.clearCounter()}>Limpiar</button>
+				<button onClick={() => this.minusClick()}>Restar</button>
+			</div>
+		);
+	}
 }
 
 function mapStateToProps(state) {
-  return {
-    counter: state.counter
-  };
+	return {
+		counter: state.counter,
+		test: state.name
+	};
 }
 export default connect(mapStateToProps)(App);
--- src/store.js
+++ src/store.js
@@ -1,24 +1,45 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 
-const InitialState = { counter: 0 };
+const InitialState = { counter: 0, name: 'Gustavo' };
 
 export const reducer = (state = InitialState, action) => {
-    switch (action.type) {
-        case "ADD_COUNTER":
-            return Object.assign({}, state, {
-                counter: state.counter + 1
-            })
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case 'ADD_COUNTER':
+			return Object.assign({}, state, {
+				counter: state.counter + 1
+			});
+		case 'SUBTRACT_COUNTER':
+			return Object.assign({}, state, {
+				counter: state.counter - 1
+			});
+		case 'CLEAR_COUNTER':
+			return Object.assign({}, state, {
+				counter: 0
+			});
+		default:
+			return state;
+	}
 };
 
 export const addCounter = () => dispatch => {
-    return dispatch({
-        type: 'ADD_COUNTER'
-    });
+	return dispatch({
+		type: 'ADD_COUNTER'
+	});
+};
+
+export const subtractCounter = () => dispatch => {
+	return dispatch({
+		type: 'SUBTRACT_COUNTER'
+	});
+};
+
+export const clearCounter = () => dispatch => {
+	return dispatch({
+		type: 'CLEAR_COUNTER'
+	});
 };
 
 export function initializeStore() {
-    return createStore(reducer);
+	return createStore(reducer, InitialState, applyMiddleware(thunkMiddleware));
 }
